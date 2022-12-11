290f81ca742da64064b995c8ae5f157a252c5aa5
Junyd Dev <junyd.dev@gmail.com>
2022-12-11 13:43:03 +0400

    Refactored onboarding slide animation code.

--- app/src/main/java/com/android/hindara/booking/app/utils/AnimationsUtil.kt
+++ app/src/main/java/com/android/hindara/booking/app/utils/AnimationsUtil.kt
@@ -3,10 +3,12 @@ package com.android.hindara.booking.app.utils
 import androidx.compose.animation.*
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraphBuilder
 import com.android.hindara.booking.app.App
 import com.google.accompanist.navigation.animation.composable
+
+private const val POSITIVE_OFFSET = 2000
+private const val NEGATIVE_OFFSET = -2000
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -19,13 +21,13 @@ fun OnboardingContentSlideAnimation(
             if (targetState > initialState) {
                 // If the target number is larger, it slides in and fades in
                 // while the initial (smaller) number slides out and fades out.
-                slideInHorizontally { width -> width } + fadeIn() with
-                        slideOutHorizontally { height -> -height } + fadeOut()
+                slideInHorizontally { width -> getEnterTransitionOffset(width) } + fadeIn() with
+                        slideOutHorizontally { height -> getExitTransitionOffset(height) } + fadeOut()
             } else {
                 // If the target number is smaller, it slides in and fades in
                 // while the initial number slides out and fades out.
-                slideInHorizontally { width -> -width } + fadeIn() with
-                        slideOutHorizontally() { height -> height } + fadeOut()
+                slideInHorizontally { width -> getExitTransitionOffset(width) } + fadeIn() with
+                        slideOutHorizontally { height -> getEnterTransitionOffset(height) } + fadeOut()
             }.using(
                 // Disable clipping since the faded slide-in/out should
                 // be displayed out of bounds.
@@ -61,30 +63,30 @@ fun NavGraphBuilder.animatedComposable(
     }
 }
 
-private fun exitTransitionAnimation() =
-    if (App.IS_RTL) {
-        slideOutHorizontally(targetOffsetX = { 2000 })
-    }else{
-        slideOutHorizontally(targetOffsetX = { -2000 })
-    }
+private fun exitTransitionAnimation() = if (App.IS_RTL) {
+    slideOutHorizontally(targetOffsetX = { POSITIVE_OFFSET })
+} else {
+    slideOutHorizontally(targetOffsetX = { NEGATIVE_OFFSET })
+}
 
-private fun getEnterTransitionAnimation() =
-    if (App.IS_RTL) {
-        slideInHorizontally(initialOffsetX = { -2000 })
-    } else {
-        slideInHorizontally(initialOffsetX = { 2000 })
-    }
+private fun getEnterTransitionAnimation() = if (App.IS_RTL) {
+    slideInHorizontally(initialOffsetX = { NEGATIVE_OFFSET })
+} else {
+    slideInHorizontally(initialOffsetX = { POSITIVE_OFFSET })
+}
 
-private fun getPopEnterTransitionAnimation() =
-    if (App.IS_RTL) {
-        slideInHorizontally(initialOffsetX = { 2000 })
-    } else {
-        slideInHorizontally(initialOffsetX = { -2000 })
-    }
+private fun getPopEnterTransitionAnimation() = if (App.IS_RTL) {
+    slideInHorizontally(initialOffsetX = { POSITIVE_OFFSET })
+} else {
+    slideInHorizontally(initialOffsetX = { NEGATIVE_OFFSET })
+}
 
-private fun getPopExitTransitionAnimation() =
-    if (App.IS_RTL) {
-        slideOutHorizontally(targetOffsetX = { -2000 })
-    } else {
-        slideOutHorizontally(targetOffsetX = { 2000 })
-    }
+private fun getPopExitTransitionAnimation() = if (App.IS_RTL) {
+    slideOutHorizontally(targetOffsetX = { NEGATIVE_OFFSET })
+} else {
+    slideOutHorizontally(targetOffsetX = { POSITIVE_OFFSET })
+}
+
+private fun getExitTransitionOffset(offset: Int) = if (App.IS_RTL) { offset } else { -offset }
+
+private fun getEnterTransitionOffset(offset: Int) = if (App.IS_RTL) { -offset } else { offset }
